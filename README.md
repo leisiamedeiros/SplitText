74e6079b540941d061a86d47fb06eb87b27addc4
leisiamedeiros <leisiamedeiros@gmail.com>
2022-07-18 23:02:49 -0300

    update readme with print

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SplitText
 Source code written in Scala similar to BrokenText repository
+
+    .
+    ├── src                
+    │   ├── main  
+    |   |   ├── scala
+    │   ├── test
+    |   |   ├── scala
+    |   └── ...
+    └── ...
+
+## Reference
+
+[Chalenge](https://github.com/VigilTech/backend-challenge)
+
+## First Code Written C#
+[Chalenge](https://github.com/leisiamedeiros/BrokenText)
+
+## Scala Test Execution
+![image](https://user-images.githubusercontent.com/10652534/179646653-65bc9761-7660-4d0e-b95f-193850a65565.png)
--- src/test/scala/HandleTextSuite.scala
+++ src/test/scala/HandleTextSuite.scala
@@ -2,7 +2,7 @@ import HandleText.{getWordsByDelimiter, printList, splitTextBySpace}
 
 class HandleTextSuite extends munit.FunSuite {
   val words = "In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel. He wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler."
-  val maxChar = 20
+  val maxChar = 40
 
   test("validate each length") {
     val res = getWordsByDelimiter(splitTextBySpace(words), maxChar)
